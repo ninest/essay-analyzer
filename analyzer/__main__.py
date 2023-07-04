dad262d307e3c0e98c14a585d14d0190b4d2056e
ninest <parth.kabra@gmail.com>
2023-07-03 21:12:20 -0400

    Remove newlines

--- analyzer/__main__.py
+++ analyzer/__main__.py
@@ -3,10 +3,15 @@ from .repeated_words import get_repeated_word_map
 
 
 def main():
-    essay = input("Enter essay: ")
+    essay = """This is the first paragraph.
+
+This has been paragraphed second.
+
+Here is the third one for good measure."""  # input("Enter essay: ")
+    essay = essay.replace("\n", "")
     repeated = get_repeated_word_map(essay)
     readability = get_readability(essay)
-    
+
     print("Repeated words: \n", repeated)
     print("\nReadability: \n", readability)
 
